476a458a7aa615b38dd493ad67b947d99c73a40f
balintataw <balintataw2@gmail.com>
2018-03-13 20:47:38 -0700

    fix buttons on css carousel

--- main.js
+++ main.js
@@ -4,14 +4,39 @@ $(document).ready(function() {
         var targetElement = $('div', this);
         if ($('.acc-item-content').hasClass('show-toggle')) {
             $('.acc-item-content').removeClass('show-toggle')
-        }
+        } 
         targetElement.toggleClass('show-toggle')
+        
     });
 
-    // carousel
-    $.each(carouselImages, function(item) {
-        console.log('yay')
-        $('.carousel-container').append()
-    })
+    // carousel no `fadeIn` or `fadeOut` or `animate`
+    var slideIndex = 0;
+    showSlides();
+
+    function showSlides() {
+        
+        var slides = $(".mySlides");
+        
+        $.each(slides, function(i, item) {
+            item.style.display = "none";
+        })
+        slideIndex++;
+        if (slideIndex > slides.length) {slideIndex = 1}    
+        
+        slides[slideIndex-1].style.display = "block";  
+        setTimeout(showSlides, 3000); 
+    }
+
+    // slick carousel
+    $('.fat-class').slick({
+        dots: true,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        fade: true,
+        speed: 500,
+        pauseOnHover: true,
+        cssEase: 'linear'
+      });
 
 })
